f783540d672282cf5f6e02871e58aaa4e6a8320d
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-03-25 17:45:46 -0300

    spacing corrected in aboutme and portfolio

--- react-portfolio/src/components/portfolio/portfolio.jsx
+++ react-portfolio/src/components/portfolio/portfolio.jsx
@@ -5,15 +5,15 @@ import './portfolio.css'
 function Portfolio() {
   return (
 
-<section className='section-background-turq  top-bottom-padding relative  '>
+<section className='section-background-turq  section-padding relative  '>
  <div class="custom-shape-divider-top-1711386641">
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
         <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
     </svg>
 </div>
     <div>
-     <div className='space'></div>
-<Row >
+     <div className='top-space'></div>
+<Row>
  
  <Col md={4}>
   
@@ -124,7 +124,7 @@ function Portfolio() {
       </Card.Body>
     </Card>
     </Col>
-    
+    <div className='bottom-space'></div>
     </Row>
 
     </div>
