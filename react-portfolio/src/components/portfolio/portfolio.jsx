2b8a0e0a66e71518b4fbee05377595610ca0aa32
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-03-25 03:22:43 -0300

    curves in portfolio section

--- react-portfolio/src/components/portfolio/portfolio.jsx
+++ react-portfolio/src/components/portfolio/portfolio.jsx
@@ -5,9 +5,9 @@ import './portfolio.css'
 function Portfolio() {
   return (
 
-<section className='section-background-turq  padding-top padding-bottom  '>
+<section className='section-background-turq  padding-top padding-bottom   '>
  
-    <div>
+    <div className='r'>
       
 <Row >
  
@@ -125,7 +125,11 @@ function Portfolio() {
     </Row>
 
     </div>
-    
+    <div class="custom-shape-divider-bottom-1711347418">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+    </svg>
+</div>
     </section>
    
    
