e4f981d4c4fe137f9530ea52a74243f808c9cecf
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-03-27 18:07:16 -0300

    pink-orange curve transition, contact and resume files

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -6,6 +6,8 @@ import './App.css'
 
 import Aboutme from './components/aboutme/aboutme.jsx';
 import Portfolio from './components/portfolio/portfolio.jsx';
+import Contact from './components/contact/contact.jsx';
+// import Resume from './components/resume/resume.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,6 +19,8 @@ function App() {
     <Header />
     <Aboutme />
     <Portfolio />
+    <Contact />
+    {/* <Resume /> */}
       
       
     </div>
