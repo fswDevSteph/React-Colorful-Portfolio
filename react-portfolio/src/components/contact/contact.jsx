eaf1c9be37513e8aa339e23c269b114d9e6cbbbc
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-03-26 23:52:57 -0300

    created contact, resume, footer folders. jsx and css files for each. imported initials top of  jsx files

---
+++ react-portfolio/src/components/contact/contact.jsx
@@ -0,0 +1,5 @@
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import './contact.css'
