e97e71d353c88d7795a9d5ad83d177d3652329bd
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-03-30 17:50:08 -0300

    added footer and played with styling

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,28 +1,20 @@
-import { useState } from 'react'
-// import Message from './components/Message.jsx'
-import Header from './components/header/header.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
+import Header from './components/header/header.jsx'
 import Aboutme from './components/aboutme/aboutme.jsx';
 import Portfolio from './components/portfolio/portfolio.jsx';
 import Contact from './components/contact/contact.jsx';
-// import Resume from './components/resume/resume.jsx';
+import Footer from './components/footer/footer.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  
-    
-      <div >
-    <Header />
-    <Aboutme />
-    <Portfolio />
-    <Contact />
-    {/* <Resume /> */}
-      
-      
+    <div >
+      <Header />
+      <Aboutme />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   )
 }
